2022c9c0db948b2d7d1ac2b90fea70dcf68d9f42
ParthaSarathiChakraborty <parthagsarathim@gmail.com>
2022-04-24 18:51:28 +0530

    LEFT JOIN LEETCODE

---
+++ LeetCode02Joins.sql
@@ -0,0 +1,76 @@
+/*Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+   */
+
+-- SLOUTION TO THE ABOVE PROBLEM
+
+SELECT customers.name as 'Customers'  
+FROM customers
+WHERE customers.id not in  
+(                                       -- IT IS A QUERY WITHIN A QUERY(SUB-QUERY)
+    SELECT customerid FROM orders
+);
+
+
+-- ANOTHER SOLUTION USING THE LEFT JOIN
+
+SELECT Name AS Customers FROM Customers
+LEFT JOIN Orders ON Customers.Id = CustomerId
+WHERE Orders.Id IS NULL
+
