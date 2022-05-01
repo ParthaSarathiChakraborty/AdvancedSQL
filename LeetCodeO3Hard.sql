f228d03ea7c1227cbb4c7676087559b63a6118e3
PSarathiChakraborty <parthagsarathim@gmail.com>
2022-05-01 21:29:08 +0530

    Update LeetCodeO3Hard.sql
    New Commit

--- LeetCodeO3Hard.sql
+++ LeetCodeO3Hard.sql
@@ -1,4 +1,5 @@
 /*
+
 Table: Employees
 
 +-------------+---------+
@@ -47,7 +48,6 @@ Explanation:
 The employees with IDs 2 and 8 get 0 bonus because they have an even employee_id.
 The employee with ID 3 gets 0 bonus because their name starts with 'M'.
 The rest of the employees get a 100% bonus.
-Accepted 16,965     Submissions 18,881
 
 */
 
