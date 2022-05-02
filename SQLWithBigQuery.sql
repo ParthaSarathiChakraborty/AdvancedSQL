25f2a25106837a2d7d82349f6ad2090280d18c7b
PSarathiChakraborty <parthagsarathim@gmail.com>
2022-05-02 20:45:07 +0530

    SQL with BigQuery in google cloud

---
+++ SQLWithBigQuery.sql
@@ -0,0 +1,14 @@
+/*
+To use BigQuery, we'll import the Python package below:
+
+"from google.cloud import bigquery"
+
+The first step in the workflow is to create a Client object. As you'll soon see, this Client object will play a central role in retrieving information from BigQuery datasets.
+
+# Create a "Client" object
+
+"client = bigquery.Client()"
+
+Using Kaggle's public dataset BigQuery integration.
+
+*/
